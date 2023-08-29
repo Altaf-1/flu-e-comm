1e4e26be97db1a15509a1bbffb4edf3830c259ce
Altaf-1 <hussainaltafali35@gmail.com>
2023-08-29 08:14:59 +0530

    first commit

---
+++ lib/core/constants/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  MyColors._();
+
+  static const Color primaryColor = Colors.red;
+  static const Color secondaryColor = Color.fromARGB(255, 54, 244, 82);
+  
+}
